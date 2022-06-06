6cbacb55265e40fa1a4317d39bb81b86c3bacb91
Felix <80686266+wenzfe@users.noreply.github.com>
2022-06-06 14:23:22 +0200

    testing

---
+++ src/spider/main.py
@@ -0,0 +1,9 @@
+from queue import Queue
+import concurrent.futures as cf
+import argparse
+
+def main():
+    print("hi")
+
+if __name__ == '__main__':
+    main()
--- src/spider/worker.py
+++ src/spider/worker.py
@@ -1,4 +1,23 @@
+from requests_html import AsyncHTMLSession
+import asyncio
 
-# input url
 
-# output [ html, css, js, images ]
+
+
+
+
+async def main():
+    # input url
+    url = "https://www.google.com"
+    print("----")
+
+    asession = AsyncHTMLSession()
+    r = await asession.get(url)
+    print(r.html.links)
+
+    # output [ html, css, js, images ]    
+    return
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
