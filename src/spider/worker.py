16dfc23a9f3b2ea47224329c771602f7f70c5a39
Felix <80686266+wenzfe@users.noreply.github.com>
2022-06-06 21:30:55 +0200

    Update worker.py

--- src/spider/worker.py
+++ src/spider/worker.py
@@ -1,23 +1,34 @@
 from requests_html import AsyncHTMLSession
 import asyncio
+import json
 
 
-
-
-
-
-async def main():
-    # input url
-    url = "https://www.google.com"
-    print("----")
+async def crawl(url):
+    """
+    Crawl a url
+    Arguments:
+        url: string
+    Returns:
+        JSON Object with links, html, css, js, images
+    """
 
     asession = AsyncHTMLSession()
+    
     r = await asession.get(url)
-    print(r.html.links)
+    for e in r.html.find('style'): # Link tag geht auch
+        print(e)
 
-    # output [ html, css, js, images ]    
-    return
+
+
+
+    return {
+        'links': r.html.links,
+        'html': r.html.html,
+        'css': None,
+        'js': None,
+        'images': None
+    } 
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(crawl("www.google.com"))
