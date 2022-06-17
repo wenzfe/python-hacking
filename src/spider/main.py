d250515009ba42bacffc6a56574ac56283f81eaf
Felix <80686266+wenzfe@users.noreply.github.com>
2022-06-17 15:44:01 +0200

    add argparse madness to script
    * ToDo: connect argparse to script

--- src/spider/main.py
+++ src/spider/main.py
@@ -7,10 +7,10 @@ from time import sleep
 import argparse
 import logging
 import sys
+import os
 
 # import code
 from worker import thread_worker
-import os
 
 
 # ToDo: define Request Header
@@ -23,8 +23,7 @@ LOG_LEVEL = 10
 FORMAT = '[%(asctime)s] [%(thread)-6d] [%(threadName)-25s] [%(funcName)-15s] [%(levelname)-8s] [%(message)s]'
 queue_urls = Queue()
 
-# Thread safety
-# https://stackoverflow.com/questions/2227169/are-python-built-in-containers-thread-safe
+
 lock_for_Set = Lock()
 visited = set()
 # ToDo: 
@@ -68,6 +67,109 @@ def main():
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""
+    A multithreaded Web Crawler / Spider
+
+        """,
+        epilog="""
+    Level     | Numeric value 
+    --------------------------
+    CRITICAL  |   50
+    ERROR     |   40
+    WARNING   |   30
+    INFO      |   20
+    DEBUG     |   10
+    NOTSET    |    0
+        """
+
+    )
+
+    # Software Version
+    parser.add_argument('-v', '--version', action='version', 
+        version='%(prog)s version: 1.0 \n  https://www.github.com/wenzfe/python-hacking'
+    )
+
+    # Number of workers
+    parser.add_argument('-w', '--worker',
+        default=5,
+        type=int,
+        metavar='N',
+        help='Number of workers / threads to use (default: %(default)s)',
+    )
+
+    # Visit external URL's
+    parser.add_argument('-visit-ext',
+        action=argparse.BooleanOptionalAction,
+        type=bool,
+        default=False,
+        help='Set flag to visit external URLs (default: %(default)s)'
+    )
+
+    # Path Data Storage
+    parser.add_argument('-p', '--path',
+        default=os.getcwd(),
+        nargs='?',
+        help='The location on your system to store the data (default: %(default)s)'
+    )
+
+    # Log level
+    parser.add_argument('--log-lvl',
+        default=30,
+        type=int,
+        help='The logging level of the script, control the verbosity of output (default: %(default)s)'
+    )
+
+    # Log destination file or console
+    parser.add_argument('--log-output',
+        nargs='?',
+        default=sys.stdout,
+        type=argparse.FileType('w'),
+        help='The logging output destination (default: stdout)'
+    )
+
+    parser.add_argument('-urls',
+        nargs='+',
+        # required=True,
+        metavar='URL',
+        type=str,
+        help="The URL's to be crawled, in the form http://example.com:80"
+    )
+
+    parser.add_argument('--url-paths',
+        nargs='+',
+        type=str,
+        metavar='PATH',
+        help='The paths to also use when starting to performe the crawl. Can be absolute or relative'
+    )
+
+    parser.add_argument('--request-timeout',
+        type=int,
+        metavar='SEC',
+        default=60,
+        help='Number of seconds before request timeout (default: %(default)s)'
+    )
+
+    parser.add_argument('--request-proxy',
+        type=str,
+        metavar="IP",
+        help="Specify a proxy to use as a intermediate for requests"
+    )
+
+    parser.add_argument('--store-data', # allows multiple times the same value!
+        nargs='+',
+        choices=['OTHER', 'HTML', 'JS', 'CSS', 'IMAGE'],
+        default=['OTHER', 'HTML', 'JS', 'CSS', 'IMAGE'],
+        help='Select the data types to be stored'
+    )
+
+    args = parser.parse_args()
+
+
+
+
     logging.basicConfig(filemode="w", filename="spider.log", encoding='utf-8', format=FORMAT, level=LOG_LEVEL)
     # stream=sys.stdout, 
     main()
